002b1dfc75e2e805275c4d6351e6c85ae1ae27fe
Parth Patel <54561512+protocol66@users.noreply.github.com>
2022-11-29 08:21:58 -0600

    added 32x32 model

--- utils/cifar2img.py
+++ utils/cifar2img.py
@@ -12,33 +12,43 @@ def unpickle(file):
 
 if __name__ == "__main__":
     # Load the dataset
-    cifar10 = unpickle("/home/parth/Downloads/cifar-10-python/cifar-10-batches-py/data_batch_1")
-    labels = unpickle("/home/parth/Downloads/cifar-10-python/cifar-10-batches-py/batches.meta")
+    # cifar10 = unpickle("/home/parth/Downloads/cifar-10-python/cifar-10-batches-py/data_batch_1")
+    # labels = unpickle("/home/parth/Downloads/cifar-10-python/cifar-10-batches-py/batches.meta")
+    cifar10 = unpickle(r"C:\Users\Parth\Downloads\cifar-10-batches-py\test_batch")
+    labels = unpickle(r"C:\Users\Parth\Downloads\cifar-10-batches-py\batches.meta")
+    
+    num_images = 10000
     
     # print images to files
     with open("val_input.csv", "w") as f:
-        for i in range(10000):
+        for i in range(num_images):
             # img = cifar10[b'data'][i].reshape((3, 32, 32)).transpose(1, 2, 0)
             # img = Image.fromarray(img, 'RGB')
             # create directory if it doesn't exist
-            img = cifar10[b'data'][i].reshape((3, 32, 32)).transpose(1, 2, 0)
+            img = cifar10[b'data'][i]
             # crop image to 28x28
-            img = img[2:30, 2:30]
+            # img = img[2:30, 2:30]
             # reshape to 1d array
-            img = img.reshape(1, 3*28*28)
+            # img = img.reshape(1, 3*32*32)
             # dir = f"../images/binary/{labels[b'label_names'][cifar10[b'labels'][i]].decode('utf-8')}"
             # os.makedirs(dir, exist_ok=True)
             # save image as csv
             # np.savetxt(f"{dir}/{i}.csv", img, delimiter=",", newline="")
-            for j in range((3*28*28)-1):
-                f.write(f"{img[0][j]},")
-            f.write(f"{img[0][-1]}\n")
+            # skip airplanes
+            if cifar10[b'labels'][i] == 0:
+                continue
+            
+            for j in range((3*32*32)-1):
+                f.write(f"{img[j]},")
+            f.write(f"{img[-1]}\n")
             # with open(f"{dir}/{i}.txt", 'wb') as f:
             #     f.write(img)
             
     # write labels to csv
     with open("val_output.csv", "w") as f:
-        for i in range(10000):
+        for i in range(num_images):
+            if cifar10[b'labels'][i] == 0:
+                continue
             one_hot = [0]*10
             one_hot[cifar10[b'labels'][i]] = 1
             for j in range(9):
